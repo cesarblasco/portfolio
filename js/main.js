e61484f80ece40feeac6d4cc1c7554b939fb648f
Cesar Blasco <cesarblasco50@hotmail.com>
2020-03-30 22:52:33 -0300

    Adapted menu for mobile, delete commented code

--- js/main.js
+++ js/main.js
@@ -15,12 +15,12 @@ let displayLanguagesDropdown = false;
 
 function getCurrentYear() {
   const currentDate = new Date();
-  currentYear.innerHTML = currentDate.getFullYear();
+  if (currentYear) {
+    currentYear.innerHTML = currentDate.getFullYear();
+  }
 }
 
 getCurrentYear();
-
-menuMobileBtn.addEventListener("click", toggleMobileMenu);
 
 languagesDropdownBtn.addEventListener("click", () => {
   displayLanguagesDropdown = !displayLanguagesDropdown;
@@ -38,20 +38,3 @@ languagesDropdownBtn.addEventListener("click", () => {
     : "fa-chevron-down";
   languagesDropdownIcon.classList.add(newIconClass);
 });
-
-function toggleMobileMenu() {
-  if (!showMenu) {
-    menuBtn.classList.add("close");
-    menu.classList.add("show");
-    menuNav.classList.add("show");
-    menuBranding.classList.add("show");
-    navItems.forEach(item => item.classList.add("show"));
-  } else {
-    menuBtn.classList.remove("close");
-    menu.classList.remove("show");
-    menuNav.classList.remove("show");
-    menuBranding.classList.remove("show");
-    navItems.forEach(item => item.classList.remove("show"));
-  }
-  showMenu = !showMenu;
-}
